c0bc72ed1024d224cf949896346d619ac2786229
Chuck Bryan <swampyfox@gmail.com>
2015-04-07 06:31:50 -0400

    Adding Mediator and IoC configuration
    This still needs to be configured. I am building out the infrastructure
    of the web app just enough to add MediatR and StructureMap. Then, I want
    to update the app to support integration testing so I can test queries.

---
+++ TimeTracker.Web/App_Start/EFConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Configuration;
+using System.Data.Entity;
+using TimeTracker.Web.Data;
+
+namespace TimeTracker.Web
+{
+    public static class EFConfig
+    {
+        public static void Initialize()
+        {
+            Database.SetInitializer(
+                new MigrateDatabaseToLatestVersion<AppDbContext,Migrations.Configuration>());
+        }
+    }
+}
--- TimeTracker.Web/Controllers/HomeController.cs
+++ TimeTracker.Web/Controllers/HomeController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TimeTracker.Web.Data;
 
 namespace TimeTracker.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly AppDbContext _database;
+
+        public HomeController(AppDbContext database)
+        {
+            _database = database;
+        }
+
         public ActionResult Index()
         {
             return View();
